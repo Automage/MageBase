f9ea67e5d988afd384ebd1033b236381f1b152f4
Automage <pranav92000@gmail.com>
2019-03-22 04:34:32 -0400

    Redirection and serving  static files

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def rootReroute():
+    return redirect(url_for('home'));
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
-    print("Connected!");
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
     app.run()
+
