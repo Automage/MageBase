991fa1494c0cfb879c67e2588fa9222443cd425e
Automage <pranav92000@gmail.com>
2019-03-24 05:37:56 -0400

    Created User module to experiment with templates

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 from flask import Flask, render_template, redirect, url_for
+from user import User
 
 app = Flask(__name__)
 
+users = [User('Pranav'), User('Man')]
+
+users[0].addPost('What the world runs on.',
+                 """
+                 It's simple. Love. Love is what we run on, all of us.<br/>Whether you know it or not.
+                 """)
+
+users[0].addPost('Dance!',
+                 """
+                 Follow the rhythm and don't stop. Let the music guide you, sit back and watch your body sway
+                 to the vibrations of life.
+                 """)
+
+users[1].addPost('How to live?',
+                 """
+                 Love <strong>everyone</strong> around you. Be yourself, and never let anyone tell you otherwise.   
+                 """)
+
+
 @app.route('/')
-def rootReroute():
+def root_reroute():
     return redirect(url_for('home'));
+
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', users=users)
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
