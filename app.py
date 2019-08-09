c3d28a3ab79f77286ef0b766bb3075d467f1cd52
Automage <pranav92000@gmail.com>
2019-08-10 03:06:30 +0400

    Guestbook post now sends email - Uses flask-mail extension - environment variables hold user and pass

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
+from flask_mail import Mail, Message
 from post import Post
 from db_handler import DatabaseHandler
 
 app = Flask(__name__)
+
+# Flask-mail config
+# MAIL_USERNAME, MAIL_PASSWORD, MAIL_DEFAULT_SENDER should be set in cli
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = 1
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+
+mail = Mail(app)
 
 poster = [Post('What the world runs on.',
                'Pranav',
@@ -16,36 +29,6 @@ poster = [Post('What the world runs on.',
                'Today',
                """
                Follow the rhythm and don't stop. Let the music guide you, sit back and watch your body sway to the vibrations of life.
-               """),
-          Post('Blowin\' in the wind',
-               'Bob Dylan',
-               'Today',
-               """
-               Lorem ipsum dolor sit amet, est ex laudem similique interpretaris, aeque albucius euripidis has ad, 
-               id lorem accusamus reformidans ius. Mundi alienum conceptam te nam, quodsi invidunt per ex. At est debet 
-               qualisque mnesarchum, mei ne harum sententiae. Est discere sententiae inciderint no, sea ne saepe perpetua, 
-               illum elitr usu ne. Viris oblique consulatu est te, ei mei modo dissentiunt efficiantur. Vero illud 
-               adipiscing vel te, putant invidunt mea ne.
-               """),
-          Post('Blowin\' in the wind',
-               'Bob Dylan',
-               'Today',
-               """
-               Lorem ipsum dolor sit amet, est ex laudem similique interpretaris, aeque albucius euripidis has ad, 
-               id lorem accusamus reformidans ius. Mundi alienum conceptam te nam, quodsi invidunt per ex. At est debet 
-               qualisque mnesarchum, mei ne harum sententiae. Est discere sententiae inciderint no, sea ne saepe perpetua, 
-               illum elitr usu ne. Viris oblique consulatu est te, ei mei modo dissentiunt efficiantur. Vero illud 
-               adipiscing vel te, putant invidunt mea ne.
-               """),
-          Post('Blowin\' in the wind',
-               'Bob Dylan',
-               'Today',
-               """
-               Lorem ipsum dolor sit amet, est ex laudem similique interpretaris, aeque albucius euripidis has ad, 
-               id lorem accusamus reformidans ius. Mundi alienum conceptam te nam, quodsi invidunt per ex. At est debet 
-               qualisque mnesarchum, mei ne harum sententiae. Est discere sententiae inciderint no, sea ne saepe perpetua, 
-               illum elitr usu ne. Viris oblique consulatu est te, ei mei modo dissentiunt efficiantur. Vero illud 
-               adipiscing vel te, putant invidunt mea ne.
                """)
           ]
 
@@ -61,13 +44,28 @@ def home():
     # POST: Guestbook form
     if request.method == 'POST':
         timestamp = datetime.now()
-        db.add_post(Post(request.form['title'].strip(), request.form['author'].strip(), timestamp,
-                         request.form['body'].strip()))
+        post = Post(request.form['title'].strip(), request.form['author'].strip(), timestamp,
+                    request.form['body'].strip())
+        db.add_post(post)
+        send_email(post)
         return redirect(url_for('home'))
 
     # GET:
     posts = db.get_posts()
     return render_template('home.html', posts=posts)
+
+
+def send_email(post):
+    # Sender configured in MAIL_DEFAULT_SENDER environ variable
+    msg = Message(subject='Guestbook signed!', recipients=['pranav92000@gmail.com'])
+    msg.body = f"""The following was posted on MageBase on {post.date}:
+    
+    Title: {post.title}
+    Author: {post.author}
+    
+    {post.body}
+    """
+    mail.send(msg)
 
 
 if __name__ == '__main__':
